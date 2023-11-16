218e3bf41a2a6c8ba3f057abb229b3b1d69e8cd4
Jacob Coccari <jacob@Jacobs-MacBook-Pro.local>
2023-11-15 19:08:56 -0700

    realized that the runnable protocol needed a function passed to it

--- generate_response.py
+++ generate_response.py
@@ -8,11 +8,11 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 
-def get_schema(db):
-    print(type(db.get_table_info()))
-    return db.get_table_info()
+# def get_schema(db):
+#     print(type(db.get_table_info()))
+#     return db.get_table_info()
 
-def generate_assistant_response(question, db):
+def generate_assistant_response(user_question, db):
     llm = ChatOpenAI()
 
     template = """Based on the table schema below, write a SQL query that would answer the user's question:
@@ -26,13 +26,16 @@ def generate_assistant_response(question, db):
             ("human", template),
         ]
     )
+
+    def get_schema(_):
+        return db.get_table_info()
+
     sql_response = (
-        RunnablePassthrough.assign(schema=get_schema(db))
+        RunnablePassthrough.assign(schema=get_schema)
         | prompt
         | llm.bind(stop=["\nSQLResult:"])
         | StrOutputParser()
     )
-
     template = """Based on the table schema below, question, sql query, and sql response, write a natural language response:
     {schema}
 
@@ -44,14 +47,11 @@ def generate_assistant_response(question, db):
     full_chain = (
         RunnablePassthrough.assign(query=sql_response)
         | RunnablePassthrough.assign(
-            schema=get_schema(db),
+            schema=get_schema,
             response=lambda x: db.run(x["query"]),
         )
         | prompt_response
         | llm
     )
-
-    response = full_chain.invoke({"question": question})
-    query = sql_response.invoke({"question": question})
-    print(query)
-    return response
+    response = full_chain.invoke({"question": user_question})
+    return response.content
